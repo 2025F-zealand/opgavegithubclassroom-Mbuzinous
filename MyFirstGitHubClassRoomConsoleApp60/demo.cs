7457066a6fc66bdab0fc75cc2738ae300e44b1db
Kevin <68694478+Mbuzinous@users.noreply.github.com>
2025-02-06 12:43:39 +0100

    har tilføjet public klassen demo med properties int demoVar og int demoVar2

---
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class demo
+    {
+        public int demoVar { get; set; }
+        public int demoVar2 { get; set; }
+    }
+}
